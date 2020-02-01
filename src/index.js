1af1e870673518cfe223f155e48c47df5516b407
gio1378 <gio13k@infonie.fr>
2020-02-01 20:19:14 +0100

    intégration des components style material-kit-react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+//import './index.css';
+import "assets/scss/material-kit-react.scss?v=1.8.0";
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var hist = createBrowserHistory();
+
+ReactDOM.render(  <Router history={hist}>
+    <Switch>
+      <Route path="/" component={Components} />
+    </Switch>
+  </Router>, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
