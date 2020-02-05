03d04fb6818845acf24055d44c2fd1c80652fb86
Nesly PETIT BERT <petitbert.nesly@gmail.com>
2020-02-05 20:06:09 +0100

    Ajout de la vue intervenants

---
+++ src/components/Speaker/Speaker.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {Button} from 'react-bootstrap';
+
+const Speaker = ({ details, onDelete }) => (
+    <tr>
+        <th>{details.id}</th>
+        {/* <td><img className="rounded-circle" style={{width: '40px'}} src={details.avatar} alt="activity-user"/></td>  */}
+        <td>{details.firstName}</td>
+        <td>{details.lastName}</td>
+        <td>{details.entryDate}</td>
+        <td>
+            <Button variant="secondary">
+                <i className="fa fa-eye f-22 m-r-10"/>
+            </Button>
+            <Button variant="primary">
+                <i className="fa fa-edit f-22 m-r-10"/>
+            </Button>
+            <Button variant="danger" onClick={() => onDelete(details.id)}>
+                <i className="fa fa-trash f-22 m-r-10"/>
+            </Button>
+        </td>
+    </tr>
+);
+
+export default Speaker;
