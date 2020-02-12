3a950b11a74a3a46bc6960ba3aae663755dcdf81
alihasana <hasana.ali@gmail.com>
2020-02-12 14:09:49 +0100

    change chart name as per table name

--- src/components/Staff/StaffsChart.js
+++ src/components/Staff/StaffsChart.js
@@ -1,34 +1,35 @@
 import React from 'react';
 import $ from "jquery";
 import OrgChart from "orgchart";
+import Aux from "../../hoc/_Aux";
 
 class StaffsChart extends React.Component {
     componentDidMount() {
         this.$el = $(this.el);
         let data = {
-            'name': 'Lao Lao',
+            'name': 'Edouard Yves',
             'title': 'general manager',
             'children': [
-                { 'name': 'Bo Miao', 'title': 'department manager', 'className': 'middle-level',
+                { 'name': 'Brendan Vaillant', 'title': 'department manager', 'className': 'middle-level',
                     'children': [
-                        { 'name': 'Li Jing', 'title': 'senior engineer', 'className': 'product-dept' },
-                        { 'name': 'Li Xin', 'title': 'senior engineer', 'className': 'product-dept',
+                        { 'name': 'Jean Yves', 'title': 'senior engineer', 'className': 'product-dept' },
+                        { 'name': 'Jeannine Potencier', 'title': 'senior engineer', 'className': 'product-dept',
                             'children': [
                                 { 'name': 'To To', 'title': 'engineer', 'className': 'pipeline1' },
-                                { 'name': 'Fei Fei', 'title': 'engineer', 'className': 'pipeline1' },
-                                { 'name': 'Xuan Xuan', 'title': 'engineer', 'className': 'pipeline1' }
+                                { 'name': 'Paul Vaillant', 'title': 'engineer', 'className': 'pipeline1' },
+                                { 'name': 'Edouard Pepin', 'title': 'engineer', 'className': 'pipeline1' }
                             ]
                         }
                     ]
                 },
-                { 'name': 'Su Miao', 'title': 'department manager', 'className': 'middle-level',
+                { 'name': 'Douglas Sandberg', 'title': 'department manager', 'className': 'middle-level',
                     'children': [
-                        { 'name': 'Pang Pang', 'title': 'senior engineer', 'className': 'rd-dept' },
-                        { 'name': 'Hei Hei', 'title': 'senior engineer', 'className': 'rd-dept',
+                        { 'name': 'Ronaldo Perez', 'title': 'senior engineer', 'className': 'rd-dept' },
+                        { 'name': 'Douglas Crockford', 'title': 'senior engineer', 'className': 'rd-dept',
                             'children': [
-                                { 'name': 'Xiang Xiang', 'title': 'UE engineer', 'className': 'frontend1' },
-                                { 'name': 'Dan Dan', 'title': 'engineer', 'className': 'frontend1' },
-                                { 'name': 'Zai Zai', 'title': 'engineer', 'className': 'frontend1' }
+                                { 'name': 'Ronaldo Vaillant', 'title': 'UE engineer', 'className': 'frontend1' },
+                                { 'name': 'Sheryl Sandberg', 'title': 'engineer', 'className': 'frontend1' },
+                                { 'name': 'Brendan Eich', 'title': 'engineer', 'className': 'frontend1' }
                             ]
                         }
                     ]
@@ -50,7 +51,9 @@ class StaffsChart extends React.Component {
 
     render() {
         return (
-            <div id="chart-container" ref={el => this.el = el}></div>
+            <Aux>
+                <div id="chart-container" ref={el => this.el = el}></div>
+            </Aux>
         );
     }
 }
