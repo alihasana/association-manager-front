94f0127c2877f953f03a8a45d6f1a5b45f5c1de7
alihasana <hasana.ali@gmail.com>
2020-06-19 14:36:42 +0200

    install firebase and dot env

---
+++ src/services/Firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: processs.env.FIRE_BASE_API_KEY,
+    authDomain: processs.env.FIRE_BASE_AUTH_DOMAIN,
+    databaseURL: processs.env.FIRE_BASE_DATA_BASE_URL,
+    projectId: processs.env.FIRE_BASE_PROJECT_ID,
+    storageBucket: processs.env.FIRE_BASE_STORAGE_BUCKET,
+    messagingSenderId: processs.env.FIRE_BASE_MESSAGING_SENDER_ID,
+    appId: processs.env.FIRE_BASE_APP_ID
+};
